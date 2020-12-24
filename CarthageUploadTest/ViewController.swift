8b40f7be9ad347291c02833c5822be49ec3e33a4
OzdGini <ozd@gini-apps.com>
2020-12-24 15:22:18 +0200

    Initial Commit

---
+++ CarthageUploadTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageUploadTest
+//
+//  Created by Oz Deutsch on 24/12/2020.
+//  Copyright © 2020 Cloudinary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
